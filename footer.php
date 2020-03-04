8dd6d2b6fafa1362ba9401cd54a67d277008c053
xcsoft <42080379+soxft@users.noreply.github.com>
2020-03-04 11:36:57 +0800

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<!--footer-->
-<footer>
-    <div class="mdui-divider-inset"></div>
-    <!-- 
-    请保留版权信息,谢谢配合!侵权必究!
-    -->
-    <center><p> &copy;2019 <a href='http://xsot.cn'>XCSOFT</a> All Rights Reserved.</p><center>
-</footer>
