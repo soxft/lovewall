fa3d2853552f5a9d42ccb90d0e37afea6258bd94
xcsoft <42080379+soxft@users.noreply.github.com>
2020-03-22 15:03:06 +0800

    Update function.php

--- function.php
+++ function.php
@@ -22,26 +22,4 @@ $ban = array(
   "1.1.1.2" => "ban"
 );
 //ban用户数据库
-
-function emailsend($sendto,$topic,$content) {
-  $data = array (
-    'key' => "emailapikeyxcsoft",
-    'sendto' => $sendto,
-    'topic' => $topic,
-    'content' => $content,
-    'name' => 'lovewall'
-  );
-  $ch = curl_init ();
-  curl_setopt ($ch, CURLOPT_URL, "https://xsot.cn/api/email/emailsend.php");
-  curl_setopt ($ch, CURLOPT_POST, 1);
-  curl_setopt ($ch, CURLOPT_HEADER, 0);
-  curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt ($ch, CURLOPT_POSTFIELDS, $data);
-  $return = curl_exec ($ch);
-  curl_close ($ch);
-  $arr = json_decode($return, true);
-  //获取返回值
-  return $arr;
-}
-//邮件api
   ?>
